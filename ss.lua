29e7d071fdc03bd22db9ac8091855a2a261b55bd
GENERRAL <jonmemo053@gmail.com>
2017-06-10 22:16:00 +0300

    Add files via upload

---
+++ ss.lua
@@ -0,0 +1,24 @@
+
+do
+    
+local function run(msg,matches)
+    if matches[1] == "chat_add_user"  then 
+      return "بنؤؤؤؤر   القالي 🌝✋🏻 \n".."️ اسم الضافك 👥 \n"..(msg.from.first_name or " ").."\n".."تابع شعرات صدري 🌞👈🏻{@X_69_X}"
+    elseif matches[1] == "chat_add_user_link" then
+      return "بنؤؤؤؤؤر القالي 🌝✋🏻\n"..(msg.from.first_name or " ").."\n".."تابع سعرات صدري 🌞👈🏻{@X_69_X}"
+
+    end
+    if matches[1] == "chat_del_user" then
+    return '#ديييييييييييييييي🌝🍆\n'..msg.action.user.first_name..'\n'
+end
+end
+return {
+    patterns = {
+        "^!!tgservice (chat_add_user)$",
+        "^!!tgservice (chat_add_user_link)$",
+        "^!!tgservice (chat_del_user)$"
+       
+    },
+ run = run,
+}
+end
